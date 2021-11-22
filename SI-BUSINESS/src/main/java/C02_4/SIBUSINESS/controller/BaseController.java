0b50bd856db44dc022926c3bf071ccb1f2691144
marsha.prawira <marsha.prawira@pinhome.id>
2021-11-23 00:44:57 +0700

    feat: landing page done

---
+++ SI-BUSINESS/src/main/java/C02_4/SIBUSINESS/controller/BaseController.java
@@ -0,0 +1,12 @@
+package C02_4.SIBUSINESS.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BaseController {
+    @GetMapping("/")
+    private String home() {
+        return "home";
+    }
+}
