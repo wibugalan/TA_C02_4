795cdcc2a15333f0c332125ae57f8107f9271d94
marsha.prawira <marsha.prawira@pinhome.id>
2021-11-22 22:27:31 +0700

    first commit

---
+++ SI-BUSINESS/src/main/java/C02_4/SIBUSINESS/SiBusinessApplication.java
@@ -0,0 +1,13 @@
+package C02_4.SIBUSINESS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiBusinessApplication.class, args);
+	}
+
+}
---
+++ SI-BUSINESS/src/test/java/C02_4/SIBUSINESS/SiBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package C02_4.SIBUSINESS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
