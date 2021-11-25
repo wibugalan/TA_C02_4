1967d1954dbca4b2a8e1cf43d4cedf32ffa23ca8
Joshua Rihi <joshuarihi8@gmail.com>
2021-11-25 15:20:23 +0700

    feature:membuat fitur login dan logout

--- src/main/java/C02_4/SIBUSINESS/controller/BaseController.java
+++ src/main/java/C02_4/SIBUSINESS/controller/BaseController.java
@@ -2,11 +2,17 @@ package C02_4.SIBUSINESS.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BaseController {
-    @GetMapping("/")
-    private String home() {
+    @RequestMapping("/")
+    public String home() {
         return "home";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
     }
 }
---
+++ src/main/java/C02_4/SIBUSINESS/security/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package C02_4.SIBUSINESS.security;
+
+import C02_4.SIBUSINESS.model.UserModel;
+import C02_4.SIBUSINESS.repository.UserDB;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserDB userDb;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserModel user = userDb.findByUsername(username);
+
+        Set<GrantedAuthority> grantedAuthoritySet = new HashSet<>();
+        grantedAuthoritySet.add (new SimpleGrantedAuthority(user.getRole().getRole_name()));
+        return new User(user.getUsername(), user.getPassword(), grantedAuthoritySet);
+
+    }
+}
