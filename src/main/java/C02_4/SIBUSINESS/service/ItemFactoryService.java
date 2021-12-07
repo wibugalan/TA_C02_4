cd22f97706498a82175aa4ed1c96bbf3aa1ab33b
Joshua Rihi <joshuarihi8@gmail.com>
2021-12-07 23:24:22 +0700

    feature: menambahkan halaman untuk feature 14

---
+++ src/main/java/C02_4/SIBUSINESS/service/ItemFactoryService.java
@@ -0,0 +1,9 @@
+package C02_4.SIBUSINESS.service;
+
+import C02_4.SIBUSINESS.model.ItemFactoryModel;
+
+import java.util.List;
+
+public interface ItemFactoryService {
+    List<ItemFactoryModel> getItemRequestList();
+}
