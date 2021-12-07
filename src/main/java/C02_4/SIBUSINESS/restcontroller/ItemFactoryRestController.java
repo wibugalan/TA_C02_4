301f3c29160afc97a7097cbc38c6687c054f7c7e
Joshua Rihi <joshuarihi8@gmail.com>
2021-12-07 14:53:36 +0700

    feature: mengerjakan feature 14 webservice request pembuatan Item

---
+++ src/main/java/C02_4/SIBUSINESS/restcontroller/ItemFactoryRestController.java
@@ -0,0 +1,36 @@
+package C02_4.SIBUSINESS.restcontroller;
+
+import C02_4.SIBUSINESS.model.ItemFactoryModel;
+import C02_4.SIBUSINESS.service.ItemFactoryRestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Mono;
+
+
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/api/item")
+public class ItemFactoryRestController {
+
+    @Autowired
+    ItemFactoryRestService itemFactoryRestService;
+
+    @PostMapping(value="")
+    private ItemFactoryModel createItem(@Valid @RequestBody ItemFactoryModel item, BindingResult bindingResult){
+        if(bindingResult.hasFieldErrors()){
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, "Request body has invalid type or missing field."
+            );
+        }else{
+            return itemFactoryRestService.createItem(item);
+        }
+    }
+}
---
+++ src/main/java/C02_4/SIBUSINESS/service/ItemFactoryRestService.java
@@ -0,0 +1,7 @@
+package C02_4.SIBUSINESS.service;
+
+import C02_4.SIBUSINESS.model.ItemFactoryModel;
+
+public interface ItemFactoryRestService {
+    ItemFactoryModel createItem(ItemFactoryModel item);
+}
