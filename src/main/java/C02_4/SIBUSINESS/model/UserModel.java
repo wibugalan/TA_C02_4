23a23c710e0c3b877a0c540698d0bb34b92e243c
Joshua Rihi <joshuarihi8@gmail.com>
2021-12-09 23:38:06 +0700

    feature:menambah tombol pada home untuk feat-14

--- src/main/java/C02_4/SIBUSINESS/model/UserModel.java
+++ src/main/java/C02_4/SIBUSINESS/model/UserModel.java
@@ -36,7 +36,7 @@ public class UserModel implements Serializable{
 
     @NotNull
     @Size(max = 50)
-    @Column(name = "username", nullable = false, unique = true)
+    @Column(name = "username", nullable = false)
     private String username;
 
     @NotNull
