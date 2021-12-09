c5640313b1d86749a7472914a823d2b2e1985d85
marsha.prawira <marsha.prawira@pinhome.id>
2021-12-09 23:32:46 +0700

    feat: fix data type feature 16

--- src/main/java/C02_4/SIBUSINESS/service/CabangRestServiceImpl.java
+++ src/main/java/C02_4/SIBUSINESS/service/CabangRestServiceImpl.java
@@ -1,13 +1,18 @@
 package C02_4.SIBUSINESS.service;
 
 import C02_4.SIBUSINESS.rest.CabangDetail;
+import io.netty.handler.codec.Headers;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import org.springframework.web.reactive.function.client.WebClient.ResponseSpec;
 
 import javax.transaction.Transactional;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @Transactional
@@ -15,19 +20,23 @@ public class CabangRestServiceImpl implements CabangRestService{
     private final WebClient webClient;
 
     public CabangRestServiceImpl(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder.baseUrl("siretail-6.herokuapp.com").build();
+        this.webClient = webClientBuilder.baseUrl("https://siretail-6.herokuapp.com").build();
     }
 
     @Override
     public Mono<CabangDetail> postCabang(CabangDetail cabang) {
-        MultiValueMap<String, String> bodyRequest = new LinkedMultiValueMap<>();
-        bodyRequest.add("nama", cabang.getNama());
-        bodyRequest.add("alamat", cabang.getAlamat());
-        bodyRequest.add("ukuran", Long.toString(cabang.getUkuran()));
-        bodyRequest.add("noTelp", cabang.getNoTelp());
-        bodyRequest.add("status", Long.toString(cabang.getStatus()));
+        Map body = new HashMap();
+        body.put("nama", cabang.getNama());
+        body.put("alamat", cabang.getAlamat());
+        body.put("noTelp", cabang.getNoTelp());
+        body.put("ukuran", cabang.getUkuran());
+        body.put("status", cabang.getStatus());
 
-        WebClient.ResponseSpec start = webClient.post().uri("/api/cabang").bodyValue(bodyRequest).retrieve();
+        ResponseSpec start = webClient.post().uri("/api/cabang").bodyValue(body).retrieve();
+        Mono<String> x = start.bodyToMono(String.class);
+
+        System.out.println(x.block()+ "test");
         return  start.bodyToMono(CabangDetail.class);
     }
+
 }
