ab53beb6acd89d45332b7f9fa832d90c817224f9
fadiyalatifah <81474184+fadiyalatifah@users.noreply.github.com>
2021-12-10 22:19:58 +0700

    Feat 12 2 (#44)
    * add ui for table
    
    * change id kategori to string kategori
    
    * change url endpoint
    
    * add authority
    
    * fitur 14 kategori to id kategori
    
    * fitur 12 kategori to id kategori 2
    
    * fitur 12 filter
    
    * fitur 12 filter merge main
    
    * fix url to api/list-item
    
    * fix url to api/list-item 2
    
    * change sec
    
    * change api
    
    * change api fitur 12

--- src/main/java/C02_4/SIBUSINESS/rest/Setting.java
+++ src/main/java/C02_4/SIBUSINESS/rest/Setting.java
@@ -2,5 +2,5 @@ package C02_4.SIBUSINESS.rest;
 
 public class Setting {
     final public static  String ItemUrl = "https://si-item.herokuapp.com/";
-    final public static String factoryUrl = "https://663a590f-0eb8-48a8-965a-a0431acd1d44.mock.pstmn.io";
+    final public static String factoryUrl = "https://apap-ta-c02-5.herokuapp.com";
 }
