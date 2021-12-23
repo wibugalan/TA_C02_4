041ad3bc2e926283174795c6eee1af110dd44708
Muhammad Marsha Prawira <75771815+wibugalan@users.noreply.github.com>
2021-12-23 22:47:02 +0700

    Feat 11 (#57)
    * feat 11: delete coupon
    
    * feat: implement feature 11 on coupon detail page
    
    * add coupon code generator
    
    * fix: fix coupontype and coupon when assigned to none
    
    Co-authored-by: marsha.prawira <marsha.prawira@pinhome.id>

--- src/main/java/C02_4/SIBUSINESS/restcontroller/CouponRestController.java
+++ src/main/java/C02_4/SIBUSINESS/restcontroller/CouponRestController.java
@@ -1,7 +1,9 @@
 package C02_4.SIBUSINESS.restcontroller;
 
+import C02_4.SIBUSINESS.model.CouponModel;
 import C02_4.SIBUSINESS.rest.CouponDetail;
 import C02_4.SIBUSINESS.service.CouponRestService;
+import C02_4.SIBUSINESS.service.CouponService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +17,14 @@ public class CouponRestController {
     @Autowired
     CouponRestService couponRestService;
 
+    @Autowired
+    CouponService couponService;
+
     @GetMapping(value = "/list-coupon")
     private List<CouponDetail> retrieveListCoupon() {
+        for (CouponModel c : couponService.getListCoupon()) {
+            c.setCoupon_code(couponService.generateKodeCoupon(c));
+        }
         return couponRestService.getAllCoupon();
     }
 }
--- src/main/java/C02_4/SIBUSINESS/service/CouponServiceImpl.java
+++ src/main/java/C02_4/SIBUSINESS/service/CouponServiceImpl.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+<<<<<<< HEAD
+import java.text.SimpleDateFormat;
+=======
+>>>>>>> 970b8dceb50c6e962916b93dda5cc43838431543
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
@@ -58,9 +62,8 @@ public class CouponServiceImpl implements CouponService{
     }
 
     @Override
-    public String generateKodeCoupon(CouponModel coupon, List<CouponTypeModel> couponValues) {
+    public String generateKodeCoupon(CouponModel coupon) {
         String out = "DISC";
-        String date = coupon.getExpiryDate().toString();
         String discAmount = Integer.toString(Math.round(coupon.getDiscountAmount() * 100));
         if(discAmount.length()==1){
             out += "00" + discAmount;
@@ -73,8 +76,8 @@ public class CouponServiceImpl implements CouponService{
         }
 
         List<String> day = new ArrayList<>();
-        if (!couponValues.isEmpty()) {
-            for(CouponTypeModel c: couponValues) {
+        if (!coupon.getListCoupontype().isEmpty()) {
+            for(CouponTypeModel c: coupon.getListCoupontype()) {
                 day.add(c.getUse_day());
             }
 
@@ -100,47 +103,10 @@ public class CouponServiceImpl implements CouponService{
                 out += "SAT";
             }
         }
-//        out += coupon.getExpiryDate().
-        String dayy = date.substring(8, 10);
-        String month = date.substring(4,7);
-        String year = date.substring(date.length()-2, date.length());
-        System.out.println(dayy);
-        System.out.println(month);
-        System.out.println(year);
-        out += dayy;
-
-        if (month.equalsIgnoreCase("jan")) {
-            out += "01";
-        }
-        else if (month.equalsIgnoreCase("feb")) {
-            out += "02";
-        }
-        else if (month.equalsIgnoreCase("mar")) {
-            out += "03";
-        }
-        else if (month.equalsIgnoreCase("apr")) {
-            out += "04";
-        }
-        else if (month.equalsIgnoreCase("may")) {
-            out += "05";
-        }else if (month.equalsIgnoreCase("jun")) {
-            out += "06";
-        }else if (month.equalsIgnoreCase("jul")) {
-            out += "07";
-        }else if (month.equalsIgnoreCase("aug")) {
-            out += "08";
-        }else if (month.equalsIgnoreCase("sep")) {
-            out += "09";
-        }else if (month.equalsIgnoreCase("okt")) {
-            out += "10";
-        }else if (month.equalsIgnoreCase("nov")) {
-            out += "11";
-        }
-        else if (month.equalsIgnoreCase("dec")) {
-            out += "12";
-        }
-
-        out += year;
+        SimpleDateFormat formatter = new SimpleDateFormat("ddMMyyyy");
+        String strDate = formatter.format(coupon.getExpiryDate());
+        out += strDate.substring(0,4);
+        out += strDate.substring(6);
         return out;
     }
 }
